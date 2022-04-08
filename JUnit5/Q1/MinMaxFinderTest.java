f10d678e1046ed2be99895d9b80d8fd31b36a359
Ashwini 18 <ashwinisharma62001.com>
2022-04-08 19:54:50 +0530

    JUnit uploaded

---
+++ JUnit5/Q1/MinMaxFinderTest.java
@@ -0,0 +1,16 @@
+package assignment1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+public class MinMaxFinderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
